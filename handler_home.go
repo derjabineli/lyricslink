0c6f91cdf56b710eae3ec1b4060dd85c94091d4e
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-02-25 15:42:18 -0800

    home page render

---
+++ handler_home.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func handlerHome(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./tmpl/home.html")
+		if err != nil {
+			http.Error(w, "Error loading login page", http.StatusInternalServerError)
+			log.Println("Template parsing error:", err)
+			return
+		}
+	
+		err = t.Execute(w, nil)
+		if err != nil {
+			http.Error(w, "Error rendering login page", http.StatusInternalServerError)
+			log.Println("Template execution error:", err)
+		}
+}
