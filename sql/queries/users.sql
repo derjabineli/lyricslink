02f1070e239d64dca029bf5eb53cd8c855e20d00
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-02-27 17:14:21 -0800

    db users query

---
+++ sql/queries/users.sql
@@ -0,0 +1,4 @@
+-- name: CreateUser :one
+INSERT INTO users (id, first_name, last_name, email, hashed_password, created_at, updated_at, pc_authorized)
+VALUES($1, $2, $3, $4, $5, $6, $7, $8)
+RETURNING *;
