5e3f908d7d35501e3fee3f27f5c89432034db720
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-03-03 11:35:44 -0800

    login route handler

---
+++ handler_login.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type accessParameters struct {
+	GrantType string `json:"grant_type"`
+	Code string `json:"code"`
+	ClientID string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+	RedirectURI string `json:"redirect_uri"`
+}
+
+type authorizationParameters struct {
+	AccessToken string `json:"access_token"`
+	TokenType string `json:"token_type"`
+	ExpiresIn int `json:"expires_in"`
+	RefreshToken string `json:"refresh_token"`
+	Scope string `json:"scope"`
+	CreatedAt int `json:"created_at"`
+}
+
+func (cfg *config) login(w http.ResponseWriter, r *http.Request) {
+		redirectURI := url.QueryEscape(cfg.pcRedirect)
+
+		link := fmt.Sprintf(
+			"https://api.planningcenteronline.com/oauth/authorize?client_id=%v&redirect_uri=%v&response_type=code&scope=services",
+			cfg.pcClient, redirectURI,
+		)
+	
+		t, err := template.ParseFiles("./template/login.html")
+		if err != nil {
+			http.Error(w, "Error loading login page", http.StatusInternalServerError)
+			log.Println("Template parsing error:", err)
+			return
+		}
+	
+		err = t.Execute(w, link)
+		if err != nil {
+			http.Error(w, "Error rendering login page", http.StatusInternalServerError)
+			log.Println("Template execution error:", err)
+		}
+}
+
+func (cfg *config) planningcentercallback(w http.ResponseWriter, r *http.Request) {
+	parsedURL, err := url.Parse(r.RequestURI)
+	if err != nil {
+		return
+	}
+	code := parsedURL.Query().Get("code")
+	fmt.Printf("Code: %v", code)
+	accessParams := accessParameters {
+		GrantType: "authorization_code",
+		Code: code,
+		ClientID: cfg.pcClient,
+		ClientSecret: cfg.pcSecret,
+		RedirectURI: cfg.pcRedirect,
+	}
+
+	requestBody, err := json.Marshal(accessParams)
+	if err != nil {
+		panic(err)
+	}
+
+	client := &http.Client{}	
+	authReq, err := http.NewRequest("POST", "https://api.planningcenteronline.com/oauth/token", bytes.NewBuffer(requestBody))
+	if err != nil {
+		panic(err)
+	}
+	authReq.Header.Add("Content-Type", "application/json; charset=utf-8")
+	resp, err := client.Do(authReq)
+	decoder := json.NewDecoder(resp.Body)
+	authParams := authorizationParameters{}
+	decoder.Decode(&authParams)
+
+	fmt.Printf("Access token: %v\n", authParams.AccessToken)
+	
+	cookie := http.Cookie{
+		Name: "jwt-token",
+		Value: "test", 
+		Path: "/",
+	}
+
+	http.SetCookie(w, &cookie)
+	http.Redirect(w, r, "/dashboard", http.StatusPermanentRedirect)
+}
