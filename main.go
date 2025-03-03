9a130e59d4b488fac19f32993d8695ce4a0fb10d
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-03-03 11:34:41 -0800

    initial backend routes

--- main.go
+++ main.go
@@ -1,20 +1,60 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/derjabineli/lyricslink/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
+
+type config struct {
+	db *database.Queries
+	pcClient string
+	pcSecret string
+	pcRedirect string
+}
 
 
 func main() {
+	godotenv.Load()
+	pcClient := os.Getenv("PC_CLIENTID")
+	pcSecret := os.Getenv("PC_SECRET")
+	pcRedirect := os.Getenv("PC_REDIRECT")
+	dbURL := os.Getenv("DB_URL")
+  	db, err := sql.Open("postgres", dbURL)
+  	if err != nil {
+   		fmt.Print(err)
+    	return 
+  	}
+	dbQueries := database.New(db)
+
+	cfg := config{db: dbQueries, pcClient: pcClient, pcSecret: pcSecret, pcRedirect: pcRedirect}
+	
+
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", handlerStart)
+	// PAGES
+	mux.HandleFunc("/", handlerHome)
+	mux.HandleFunc("/login", cfg.login)
+	mux.HandleFunc("/static/", staticHandler)
+	mux.HandleFunc("/dashboard", handlerDashboard)
+	
+	// API
+	mux.HandleFunc("/api/register", cfg.register)
+
+	// AUTH
+	mux.HandleFunc("/pc/callback", cfg.planningcentercallback)
 
 	server := &http.Server{
 		Handler: mux,
-		Addr: ":8080",
+		Addr: ":3005",
 	  }
 	  
+	  fmt.Print("Running on Port 3005")
 	  log.Fatal(server.ListenAndServe())
 }
