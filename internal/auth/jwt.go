015a21beace2fa561edd7aae6cff6a905cc2072b
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-03-04 18:07:49 -0800

    added jwt generate function

---
+++ internal/auth/jwt.go
@@ -0,0 +1,24 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+)
+
+type TokenType string
+
+const (
+	TokenTypeAccess TokenType = "lyriclink-access"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: string(TokenTypeAccess),
+		IssuedAt: jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiresIn)),
+		Subject: userID.String(),
+	})
+	return token.SignedString([]byte(tokenSecret))
+}
