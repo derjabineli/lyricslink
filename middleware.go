896bdb0c43b16c92b2cbd31db2ae6ed066d1ceb4
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-03-10 15:59:24 -0700

    add guest only middleware to redirect depending on auth status

--- middleware.go
+++ middleware.go
@@ -1,23 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/derjabineli/lyricslink/internal/auth"
+	"github.com/google/uuid"
 )
+
+func (cfg *config) getUserIDFromCookie(r *http.Request) (uuid.UUID, error) {
+	cookie, err := r.Cookie("ll_user")
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return auth.ValidateJWT(cookie.Value, cfg.tokenSecret) // Validate JWT
+}
 
 func (cfg *config) authMiddleware(next http.HandlerFunc) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("ll_user")
+		id, err := cfg.getUserIDFromCookie(r)
 		if err != nil {
-			http.Redirect(w, r, "/", 301)
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
 			return
 		}
-		fmt.Printf("Cookie: %v\n", cookie)
-		id, err := auth.ValidateJWT(cookie.Value, cfg.tokenSecret)
-		if err != nil {}
-		fmt.Printf("Id: %v\n", id)
+
+		log.Printf("Authenticated user ID: %v", id)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *config) guestOnlyMiddleware(next http.HandlerFunc) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, err := cfg.getUserIDFromCookie(r)
+		if err == nil {
+			http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
