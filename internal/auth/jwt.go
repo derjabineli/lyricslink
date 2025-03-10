b54ca29383e3cc491b9c516c847cbba6135a1da5
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-03-10 14:49:06 -0700

    jwt validator

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -21,4 +22,34 @@ func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (str
 		Subject: userID.String(),
 	})
 	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.RegisteredClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString, 
+		&claims, 
+		func(token *jwt.Token) (interface{}, error) {return []byte(tokenSecret), nil},
+	)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	userIDString, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	issuer, err := token.Claims.GetIssuer()
+	if err != nil {
+		return uuid.Nil, err
+	}
+	if issuer != string(TokenTypeAccess) {
+		return uuid.Nil, errors.New("invalid issuer")
+	}
+	
+	id, err := uuid.Parse(userIDString)
+	if err != nil {
+		return uuid.Nil, err
+	}
+	return id, nil
 }
