5c8a4f5189c933e1ce3dabfccb08e601cfab51e7
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-03-19 12:17:14 -0700

    event edit page

---
+++ frontend/static/event.js
@@ -0,0 +1,18 @@
+function handleClick(element) {
+  arrangements = element.querySelector(".arrangements")
+  if (arrangements.classList.contains("hidden")) {
+    arrangements.classList.remove("hidden")
+    element.classList.remove("hover:bg-gray-100")
+  } else {
+    arrangements.classList.add("hidden")
+    element.classList.add("hover:bg-gray-100")
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll(".arrangements").forEach((arrangement) => {
+    arrangement.addEventListener("click", function (event) {
+      event.stopPropagation() // Prevents the event from bubbling up
+    })
+  })
+})
