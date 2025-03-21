f14b05e295e07a274a24317c72cff0d4568730e3
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-03-21 15:00:19 -0700

    add pc song route env variable

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ type config struct {
 	pcSecret string
 	pcRedirect string
 	tokenSecret string
+	pcSongRoute string
 	tokenDuration time.Duration
 }
 
@@ -29,6 +30,7 @@ func main() {
 	pcSecret := os.Getenv("PC_SECRET")
 	pcRedirect := os.Getenv("PC_REDIRECT")
 	tokenSecret := os.Getenv("TOKEN_SECRET")
+	pcSongRoute := os.Getenv("PC_SONG_ROUTE")
 	dbURL := os.Getenv("DB_URL")
   	db, err := sql.Open("postgres", dbURL)
   	if err != nil {
@@ -37,7 +39,7 @@ func main() {
   	}
 	dbQueries := database.New(db)
 
-	cfg := config{db: dbQueries, pcClient: pcClient, pcSecret: pcSecret, pcRedirect: pcRedirect, tokenSecret: tokenSecret, tokenDuration: time.Hour * 8}
+	cfg := config{db: dbQueries, pcClient: pcClient, pcSecret: pcSecret, pcRedirect: pcRedirect, tokenSecret: tokenSecret, tokenDuration: time.Hour * 8, pcSongRoute: pcSongRoute}
 	
 
 	mux := http.NewServeMux()
@@ -49,6 +51,7 @@ func main() {
 	mux.HandleFunc("/loginPC", cfg.loginPC)
 	mux.Handle("/dashboard", cfg.authMiddleware(cfg.handlerDashboard)) 
 	mux.Handle("/events/{id}", cfg.authMiddleware(cfg.handlerEvents)) 
+	mux.Handle("/settings", cfg.authMiddleware(cfg.handlerSettings)) 
 	
 	// API
 	mux.Handle("POST /api/events", cfg.authMiddleware(cfg.addEvent))
@@ -56,6 +59,7 @@ func main() {
 	mux.Handle("POST /api/songs", cfg.authMiddleware(cfg.getSongs))
 	mux.Handle("GET /api/songs/{id}", cfg.authMiddleware(cfg.getArrangements))
 	mux.Handle("POST /api/event_arrangements", cfg.authMiddleware(cfg.addArrangementToEvent))
+	mux.Handle("PUT /api/event_arrangements", cfg.authMiddleware(cfg.updateEventArrangement))
 
 	// AUTH
 	mux.HandleFunc("/pc/callback", cfg.planningcentercallback)
