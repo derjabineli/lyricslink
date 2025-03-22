7aac12c43f1ff099e5e8928a3ca8d814496f5e23
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-03-22 14:36:46 -0700

    add sql null validator utility functions

--- sql_getters.go
+++ sql_getters.go
@@ -1,6 +1,8 @@
 package main
 
-import "database/sql"
+import (
+	"database/sql"
+)
 
 func getInt32Value (n sql.NullInt32) int {
 	if n.Valid {
@@ -16,4 +18,18 @@ func getSqlStringValue (s sql.NullString) string {
 	} else {
 		return ""
 	}
+}
+
+func validateSqlNullString (s string) sql.NullString {
+	if s != "" {
+		return sql.NullString{Valid: true, String: s}
+	}
+	return sql.NullString{Valid: false}
+}
+
+func validateSqlNullInt32 (n int) sql.NullInt32 {
+	if n != 0 {
+		return sql.NullInt32{Valid: true, Int32: int32(n)}
+	}
+	return sql.NullInt32{Valid: false}
 }
