c574a5dfa9ba56134f86763df8899e14c326fd5a
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-03-26 10:29:34 -0700

    errorResponse json struct

--- json.go
+++ json.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
+
+type jsonErrorResponse struct {
+    Error string `json:"error"`
+}
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
     response, err := json.Marshal(payload)
     if err != nil {
         return err
     }
+
     w.Header().Set("Content-Type", "application/json")
     w.Header().Set("Access-Control-Allow-Origin", "*")
     w.WriteHeader(code)
-    w.Write(response)
-    return nil
+
+    _, err = w.Write(response)
+	if err != nil {
+		log.Printf("Failed to write response: %v", err)
+        return err
+	}
+	return nil
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string) error {
-    return respondWithJSON(w, code, map[string]string{"error": msg})
+    return respondWithJSON(w, code, jsonErrorResponse{Error: msg})
 }
