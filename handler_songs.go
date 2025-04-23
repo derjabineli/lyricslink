be471976f37a91970bac9270a0b743cfa288a9fc
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-04-23 11:47:54 -0700

    add sync songs function using organization id

--- handler_songs.go
+++ handler_songs.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/derjabineli/lyricslink/internal/database"
 	"github.com/google/uuid"
@@ -29,6 +31,14 @@ func (cfg *config) getSongs(w http.ResponseWriter, r *http.Request) {
 	userID, err := getUserIDFromContext(r)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Unauthorized page")
+		return
+	}
+
+	organizationID, err := cfg.db.GetOrganizationIDByUserID(r.Context(), userID)
+	if err != nil {
+		fmt.Printf("Couldn't get Org ID.\n error: %v\n", err.Error())
+		respondWithError(w, http.StatusUnauthorized, "Internal Server Error")
+		return
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -38,10 +48,12 @@ func (cfg *config) getSongs(w http.ResponseWriter, r *http.Request) {
 
 	dbSongResults, err := cfg.db.SearchSongs(context.Background(), database.SearchSongsParams{
 		Title: searchParams.Query + "%", 
-		UserID: userID,
+		OrganizationID: organizationID,
 	})
 	if err != nil {
+		fmt.Printf("Couldn't get songs.\n error: %v\n", err.Error())
 		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
 	}
 
 	songResults := []songResultParameters{}
@@ -59,4 +71,36 @@ func (cfg *config) getSongs(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, 200, songResults)
+}
+
+func (cfg *config) syncPlanningCenterSongs(w http.ResponseWriter, r *http.Request) {
+	userID, err := getUserIDFromContext(r)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+	ctx := context.Background()
+
+	organizationID, err := cfg.db.GetOrganizationIDByUserID(ctx, userID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "there was an error syncing your songs")
+		return
+	}
+
+	token, err := cfg.db.GetTokenByUserID(ctx, userID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "there was an error syncing your songs")
+		return
+	}
+
+	tokenExpiration := token.UpdatedAt.Add(time.Second*time.Duration(token.ExpiresIn))
+	if time.Now().After(tokenExpiration){
+		fmt.Println("Token expired!")
+		return
+	}
+
+	err = cfg.fetchandSyncSongs(cfg.pcSongRoute, token.AccessToken, organizationID)
+	if err != nil {
+		fmt.Printf("Error encountered error: %v\n", err.Error())
+	}
 }
