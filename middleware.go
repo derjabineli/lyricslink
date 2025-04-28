fb30726d2a59972fb9b64a4b5f2c874873e0c790
Eli Derjabin <69370608+derjabineli@users.noreply.github.com>
2025-04-28 16:52:12 -0700

    update middleware to check refresh token when needed

--- middleware.go
+++ middleware.go
@@ -13,21 +13,35 @@ type contextKey string
 
 const userIDKey contextKey = "userID"
 
-func (cfg *config) getUserIDFromCookie(r *http.Request) (uuid.UUID, error) {
-	cookie, err := r.Cookie("ll_user")
+func (cfg *config) getUserIDFromCookie(r *http.Request, tokenName string, tokenType auth.TokenType) (uuid.UUID, error) {
+	cookie, err := r.Cookie(tokenName)
 	if err != nil {
-		return uuid.Nil, err
+		return uuid.Nil, errors.New("no token found")
 	}
-	return auth.ValidateJWT(cookie.Value, cfg.tokenSecret) // Validate JWT
+	return auth.ValidateJWT(cookie.Value, cfg.tokenSecret, tokenType) // Validate JWT
 }
 
 func (cfg *config) authMiddleware(next http.HandlerFunc) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		userID, err := cfg.getUserIDFromCookie(r)
+		userID, err := cfg.getUserIDFromCookie(r, "ll_user", auth.AccessTokenType)
+		if err == nil {
+			ctx := context.WithValue(r.Context(), userIDKey, userID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+			return
+		}
+
+		userID, err = cfg.getUserIDFromCookie(r, "ll_refresh", auth.RefreshTokenType)
 		if err != nil {
 			http.Redirect(w, r, "/login", http.StatusSeeOther)
 			return
 		}
+
+		newAccessCookie, err := auth.NewAccessTokenCookie(userID, cfg.tokenSecret)
+		if err != nil {
+			return
+		}
+		http.SetCookie(w, newAccessCookie)
+
 		ctx := context.WithValue(r.Context(), userIDKey, userID)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
@@ -44,7 +58,13 @@ func getUserIDFromContext(r *http.Request) (uuid.UUID, error) {
 
 func (cfg *config) guestOnlyMiddleware(next http.HandlerFunc) http.Handler{
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		_, err := cfg.getUserIDFromCookie(r)
+		_, err := cfg.getUserIDFromCookie(r, "ll_user", auth.AccessTokenType)
+		if err == nil {
+			http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
+			return
+		}
+
+		_, err = cfg.getUserIDFromCookie(r, "ll_refresh", auth.RefreshTokenType)
 		if err == nil {
 			http.Redirect(w, r, "/dashboard", http.StatusSeeOther)
 			return
